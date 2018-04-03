5aed3f7bdb29910d18256a0939c604681111a969
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 12:58:05 +0430

    add TblFormData entity

---
+++ SampleFormGenerator.Model/Entities/TblFormData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFormGenerator.Model.Entities
+{
+    public class TblFormData
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public int Id_FormInfo { get; set; }
+    }
+}
