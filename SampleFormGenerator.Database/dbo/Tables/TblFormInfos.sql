333180436f763f1546b4513bb9a549d5527e4436
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 12:47:40 +0430

    Add tblforminfos and tblformvalues

--- SampleFormGenerator.Database/dbo/Tables/TblFormInfos.sql
+++ SampleFormGenerator.Database/dbo/Tables/TblFormInfos.sql
@@ -1,4 +1,4 @@
-﻿CREATE TABLE [dbo].[TblFormInfo] (
+﻿CREATE TABLE [dbo].[TblFormInfos] (
     [Id]    INT            IDENTITY (1, 1) NOT NULL,
     [Title] NVARCHAR (100) NOT NULL,
     CONSTRAINT [PK_TblForm] PRIMARY KEY CLUSTERED ([Id] ASC)
---
+++ SampleFormGenerator.Database/dbo/Tables/TblFormValues.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[TblFormValues] (
+    [Id]                 INT            IDENTITY (1, 1) NOT NULL,
+    [Id_ParameterTypeId] INT            NOT NULL,
+    [Id_FormData]        INT            NOT NULL,
+    [Value]              NVARCHAR (200) NOT NULL,
+    [IsValidationPassed] BIT            CONSTRAINT [DF_TblFormValues_IsValidationPassed] DEFAULT ((0)) NOT NULL,
+    CONSTRAINT [PK_TblFormValues] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TblFormValues_TblFormData] FOREIGN KEY ([Id_FormData]) REFERENCES [dbo].[TblFormData] ([Id]),
+    CONSTRAINT [FK_TblFormValues_TblParameterTypes] FOREIGN KEY ([Id_ParameterTypeId]) REFERENCES [dbo].[TblParameterTypes] ([Id])
+);
+
