b347cd96d0a3c4b165957021acb41e0f4f8310f2
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 13:09:20 +0430

    add IRepository Interface

---
+++ SampleFormGenerator.Model/Contracts/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFormGenerator.Model.Contracts
+{
+    public interface IRepository<T> where T:IEntity
+    {
+        T Save(T model);
+        T Update(T model);
+        T Delete(T model);
+        T Get(string where,object conditionValues = null);
+        List<T> Select(string where, object conditionValues = null);
+    }
+}
