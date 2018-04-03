00588ae6f22f847bc9b08f69ee8691fbd8611484
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 12:51:25 +0430

    add TblFormInfoParameters table

---
+++ SampleFormGenerator.Database/dbo/Tables/TblFormInfoParameters.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TblFormInfoParameters] (
+    [Id_FormInfo]       INT            NOT NULL,
+    [Id_ParameterTypes] INT            NOT NULL,
+    [ParamterTitle]     NVARCHAR (100) NOT NULL,
+    [IsMandotory]       BIT            NOT NULL,
+    CONSTRAINT [FK_TblFormInfoParameters_TblFormInfos] FOREIGN KEY ([Id_FormInfo]) REFERENCES [dbo].[TblFormInfos] ([Id]),
+    CONSTRAINT [FK_TblFormInfoParameters_TblParameterTypes] FOREIGN KEY ([Id_ParameterTypes]) REFERENCES [dbo].[TblParameterTypes] ([Id])
+);
+
