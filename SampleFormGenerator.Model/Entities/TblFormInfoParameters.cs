58d7c0668911156cadc289050b55642d80ff0156
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 12:59:15 +0430

    add TblFormInfoParameters

---
+++ SampleFormGenerator.Model/Entities/TblFormInfoParameters.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFormGenerator.Model.Entities
+{
+    public class TblFormInfoParameters
+    {
+        public int Id_FormInfo { get; set; }
+        public int Id_ParameterTypes { get; set; }
+        public string ParamterTitle { get; set; }
+        public bool IsMandotory { get; set; }
+        public short ParameterOrder { get; set; }
+    }
+}
