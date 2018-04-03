f29f17cde0a420ba0b27039c3d30fdbbf7f0d169
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 12:55:20 +0430

    add paramterOrder and index to TblFormInfoParameters table

--- SampleFormGenerator.Database/dbo/Tables/TblFormInfoParameters.sql
+++ SampleFormGenerator.Database/dbo/Tables/TblFormInfoParameters.sql
@@ -3,7 +3,16 @@
     [Id_ParameterTypes] INT            NOT NULL,
     [ParamterTitle]     NVARCHAR (100) NOT NULL,
     [IsMandotory]       BIT            NOT NULL,
+    [ParameterOrder]    SMALLINT       NOT NULL,
     CONSTRAINT [FK_TblFormInfoParameters_TblFormInfos] FOREIGN KEY ([Id_FormInfo]) REFERENCES [dbo].[TblFormInfos] ([Id]),
     CONSTRAINT [FK_TblFormInfoParameters_TblParameterTypes] FOREIGN KEY ([Id_ParameterTypes]) REFERENCES [dbo].[TblParameterTypes] ([Id])
 );
+
+
+
+
+GO
+CREATE NONCLUSTERED INDEX [ix_id_order_inc_idparam]
+    ON [dbo].[TblFormInfoParameters]([Id_FormInfo] ASC, [ParameterOrder] ASC)
+    INCLUDE([Id_ParameterTypes]);
 
