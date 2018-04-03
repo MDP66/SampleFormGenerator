d5e9b90a27a3e77f25a3e3167b9b18f5d079fc0d
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 13:00:56 +0430

    add TblFormValues entity

---
+++ SampleFormGenerator.Model/Entities/TblFormInfos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFormGenerator.Model.Entities
+{
+    public class TblFormInfos
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+    }
+}
---
+++ SampleFormGenerator.Model/Entities/TblFormValues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFormGenerator.Model.Entities
+{
+    public class TblFormValues
+    {
+        public int Id { get; set; }
+        public int Id_ParameterTypeId { get; set; }
+        public int Id_FormData { get; set; }
+        public string Value { get; set; }
+        public bool IsValidationPassed { get; set; }
+    }
+}
