226eb15a4f39ead728670b1d8ff921da1c355245
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 13:55:31 +0430

    add id feild to TblFormInfoParameters table

--- SampleFormGenerator.Database/dbo/Tables/TblFormInfoParameters.sql
+++ SampleFormGenerator.Database/dbo/Tables/TblFormInfoParameters.sql
@@ -1,12 +1,16 @@
 ﻿CREATE TABLE [dbo].[TblFormInfoParameters] (
+    [Id]                INT            IDENTITY (1, 1) NOT NULL,
     [Id_FormInfo]       INT            NOT NULL,
     [Id_ParameterTypes] INT            NOT NULL,
     [ParamterTitle]     NVARCHAR (100) NOT NULL,
     [IsMandotory]       BIT            NOT NULL,
     [ParameterOrder]    SMALLINT       NOT NULL,
+    CONSTRAINT [PK_TblFormInfoParameters] PRIMARY KEY CLUSTERED ([Id] ASC),
     CONSTRAINT [FK_TblFormInfoParameters_TblFormInfos] FOREIGN KEY ([Id_FormInfo]) REFERENCES [dbo].[TblFormInfos] ([Id]),
     CONSTRAINT [FK_TblFormInfoParameters_TblParameterTypes] FOREIGN KEY ([Id_ParameterTypes]) REFERENCES [dbo].[TblParameterTypes] ([Id])
 );
+
+
 
 
 
