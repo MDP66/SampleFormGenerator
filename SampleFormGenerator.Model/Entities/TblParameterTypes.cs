1c6d6099ded88e2c2882e149e73c537800bf655d
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 13:02:57 +0430

    add TblParameterTypes entity

---
+++ SampleFormGenerator.Model/Entities/TblParameterTypes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleFormGenerator.Model.Entities
+{
+    public class TblParameterTypes
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string RegexValidator { get; set; }
+        public string RegexValidatorMessage { get; set; }
+        public string EditorType { get; set; }
+        public string ViewerType { get; set; }
+    }
+}
