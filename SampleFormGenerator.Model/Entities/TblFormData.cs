6b79200fda4790bb22c48e9656be2ff9ffa335e2
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 13:05:51 +0430

    remove redundunt refrences in entities

--- SampleFormGenerator.Model/Entities/TblFormData.cs
+++ SampleFormGenerator.Model/Entities/TblFormData.cs
@@ -1,9 +1,5 @@
 ﻿using SampleFormGenerator.Model.Contracts;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SampleFormGenerator.Model.Entities
 {
