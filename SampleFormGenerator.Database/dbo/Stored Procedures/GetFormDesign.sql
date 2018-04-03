e3556a3a065edf9913711f629932ca47f89b7d8b
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 19:56:47 +0430

    add GetFormDesign SP to database

---
+++ SampleFormGenerator.Database/dbo/Stored Procedures/GetFormDesign.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[GetFormDesign]
+(
+	@Id INT
+)
+AS
+BEGIN
+	SELECT 
+		TblFormInfoParameters.Id, TblFormInfoParameters.ParamterTitle,TblFormInfoParameters.IsMandotory,TblFormInfoParameters.ParameterOrder,
+		TblParameterTypes.RegexValidator,TblParameterTypes.RegexValidatorMessage,TblParameterTypes.EditorType as [Type]
+	FROM
+		TblFormInfoParameters 
+		INNER JOIN TblParameterTypes ON TblParameterTypes.Id = TblFormInfoParameters.Id_ParameterTypes
+	WHERE
+		TblFormInfoParameters.Id_FormInfo = @Id
+	ORDER BY
+		TblFormInfoParameters.ParameterOrder
+END
