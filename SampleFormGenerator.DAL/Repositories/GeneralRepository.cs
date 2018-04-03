325c783e53f6dd6454ea6f2c8059758a0385e246
Masoud Daneshpour <masoud@daneshpour.org>
2018-04-03 14:13:15 +0430

    add GeneralRepository

---
+++ SampleFormGenerator.DAL/Repositories/GeneralRepository.cs
@@ -0,0 +1,35 @@
+﻿using SampleFormGenerator.Model.Contracts;
+
+using Dapper;
+
+using System.Data;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace SampleFormGenerator.DAL.Repositories
+{
+   public  class GeneralRepository
+    {
+        private IConnection _connection;
+        public GeneralRepository(IConnection connection)
+        {
+            _connection = connection;
+        }
+        public IDbConnection Db => _connection.CreateDbConnection();
+
+        public async Task<IEnumerable<T>> QueryAsync<T>(string query, object conditiontions = null)
+        {
+            return await Db.QueryAsync<T>(query, conditiontions);
+        }
+
+        public async Task<T> QueryFirstAsync<T>(string query, object conditiontions = null)
+        {
+            return await Db.QueryFirstAsync<T>(query, conditiontions);
+        }
+
+        public async Task<T> ExecuteAsync<T>(string query,object conditions = null)
+        {
+            return await Db.ExecuteScalarAsync<T>(query, conditions);
+        }
+    }
+}
